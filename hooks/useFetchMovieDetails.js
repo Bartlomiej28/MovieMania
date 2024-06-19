06111d939e6a7b2bafebb567d246525df7d8bb74
Bartek <bartlomiej.boczylo@gmail.com>
2024-06-19 22:00:43 +0200

    useFetchMovieDetails fix

--- hooks/useFetchMovieDetails.js
+++ hooks/useFetchMovieDetails.js
@@ -2,5 +2,5 @@
 export async function useFetchMovieDetails(id){
     const req = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`);
     const res = await req.json();
-    return res
+    return res.results
 }
